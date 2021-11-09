a4fbc9c44c0a9c9f1a46b415490cdeda8869bbb9
Laura McBride <20084164@mail.wit.ie>
2021-11-09 12:46:59 +0000

    Errors fixed

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import MovieReviewPage from "./pages/movieReviewPage";
-import { BrowserRouter, Route, Redirect, Switch, Link } from "react-router-dom";
+import { BrowserRouter, Route, Redirect, Switch} from "react-router-dom";
 import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
